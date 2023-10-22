c42e53f9ad3767e6f2cc1c6ba319971ef731e4a1
Christopher LUKOMBO <christopher.lukombo@carbon-it.com>
2023-10-22 22:52:12 +0200

    update

--- .history/src/main/java/com/carbon/todobackend/dao/TodoRepository_20231022224906.java
+++ .history/src/main/java/com/carbon/todobackend/dao/TodoRepository_20231022224906.java
@@ -4,7 +4,7 @@ import com.carbon.todobackend.domain.entities.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
-test
+
 public interface TodoRepository extends JpaRepository<TodoEntity, UUID> {
     boolean existsByTitle(String title);
 }
